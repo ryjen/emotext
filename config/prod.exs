8365c639642d80ca673a3c2c909980d91346047a
Ryan Jennings <ryan@arg3.com>
2016-02-28 23:24:02 -0800

    errr

--- config/prod.exs
+++ config/prod.exs
@@ -48,6 +48,6 @@ config :logger, level: :info
 #
 
 config :emotext, Emotext.Repo,
-  adapter: Mongo.ecto,
+  adapter: Mongo.Ecto,
   url: System.get_env("DATABASE_URL"),
   pool_size: 20 # The amount of database connections in the pool
