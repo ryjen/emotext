ccf56617b56abb1921637b1db8f060e8b96813f2
Ryan Jennings <ryan@arg3.com>
2016-04-01 22:04:15 -0700

    update prod config

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,8 @@ config :emotext, Emotext.Endpoint,
   http: [port: System.get_env("PORT")],
   url: [scheme: "https", host: System.get_env("HOST"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+ # cache_static_manifest: "priv/static/manifest.json"
 
 # ## SSL Support
 #
