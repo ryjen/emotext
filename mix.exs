ad5bcafb8c4a385aca2e02e8c3397bc94a9880b0
Ryan Jennings <ryan@arg3.com>
2016-07-07 21:10:14 -0700

    update mix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Emotext.Mixfile do
   def project do
     [app: :emotext,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.2.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
