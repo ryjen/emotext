fa00124e30552649db2ecd88eba8e0c3816827c3
Ryan Jennings <rybertjen@gmail.com>
2022-05-15 16:46:16 -0700

    upgrading fixes

--- brunch-config.js
+++ brunch-config.js
@@ -3,28 +3,10 @@ exports.config = {
   files: {
     javascripts: {
       joinTo: "js/app.js"
-      // To use a separate vendor.js bundle, specify two files path
-      // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
-      //joinTo: {
-      //  'js/app.js': /^(web\/static\/js)/,
-      //  'js/vendor.js': /^(web\/static\/vendor)/
-      //}
-      //
-      // To change the order of concatenation of files, explictly mention here
-      // https://github.com/brunch/brunch/tree/master/docs#concatenation
-      // order: {
-      //   before: [
-      //     'web/static/vendor/js/jquery-2.1.1.js',
-      //     'web/static/vendor/js/bootstrap.min.js'
-      //   ]
-      // }
     },
     stylesheets: {
       joinTo: "css/app.css"
     },
-    templates: {
-      joinTo: "js/app.js"
-    }
   },
 
   conventions: {
